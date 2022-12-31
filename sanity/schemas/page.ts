90224538f26c72bb3aeeefd3412f5075b3573bd9
Chris LaRocque <larocque.christopher@gmail.com>
2022-12-31 10:22:08 -0500

    Adds image to page type in sanity

--- sanity/schemas/page.ts
+++ sanity/schemas/page.ts
@@ -7,6 +7,13 @@ export default {
       name: 'slug',
       type: 'string',
       title: 'Slug',
+      description: 'Path for page, ie convergencebooks.com/SLUG',
+    },
+    {
+      name: 'heroImage',
+      type: 'image',
+      title: 'Hero image',
+      description: 'Image at top of page',
     },
   ],
 }
