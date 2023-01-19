d52572fc139ca3fca4a73dfa7eea917125d25e1f
Chris LaRocque <larocque.christopher@gmail.com>
2023-01-18 22:38:14 -0500

    Adds sanity image

--- site/sanity/index.ts
+++ site/sanity/index.ts
@@ -1,13 +1,26 @@
 import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
 const client = sanityClient({
 	projectId: import.meta.env.SANITY_PROJECT_ID,
 	dataset: "production",
 	apiVersion: "2021-10-21",
 	useCdn: true,
 });
+const builder = imageUrlBuilder(client);
 
 export async function pageBySlug(slug: string) {
 	return await client.fetch(`*[slug == "${slug}"]`).then((page) => {
 		return page;
 	});
+}
+type SourceObject = {
+	_type: string;
+	asset: {
+		_ref: string;
+		_type: string;
+	};
+};
+export function urlFor(source: SourceObject) {
+	return builder.image(source);
 }
